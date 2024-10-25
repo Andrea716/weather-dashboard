fc44f0ee0e27d757e58b3d2989fabf1038674daa
Andrea716 <andreavisagie07@gmail.com>
2024-10-25 18:47:05 +0200

    Create ErrorMessage.jsx

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,8 @@
+const ErrorMessage = ({ message }) => (
+    <div className="p-4 bg-red-500 text-white rounded-md text-center">
+      {message}
+    </div>
+  );
+  
+  export default ErrorMessage;
+  
